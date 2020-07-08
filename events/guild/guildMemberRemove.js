e49e62d4f49708380adffd1fdb0be5baa3636d38
shinosaki <dermhioasw123@gmail.com>
2020-07-08 21:12:03 +0700

    Exceptions for bots

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,12 +1,16 @@
-module.exports = async message => {
+module.exports = async (client, member) => {
 	let notification = member.guild.channels.cache.find(ch => ch.name === "│การแจ้งเตือน🔔");
-	const embed = {
-		"title": member.user.tag,
-		"description": "ได้หนีออกจากเซิร์ฟเวอร์นี้ไปแล้วว...(ไปไหนของเขากันน้าาา..)",
-		"color": 16777215,
-		"thumbnail": {
-			"url": member.user.displayAvatarURL(),
-		},
-	};
-	notification.send({ embed });
+	if (member.user.bot) {
+		return;
+	} else {
+		const embed = {
+			"title": member.user.tag,
+			"description": "ได้หนีออกจากเซิร์ฟเวอร์นี้ไปแล้วว...(ไปไหนของเขากันน้าาา..)",
+			"color": 16777215,
+			"thumbnail": {
+				"url": member.user.displayAvatarURL(),
+			}
+		};
+		notification.send({ embed });
+	}
 };
