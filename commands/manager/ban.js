7b696cbef8ad2f539463fe842692d5670774331d
shinosaki <dermhioasw123@gmail.com>
2020-07-08 21:11:37 +0700

    Bug fixes

--- commands/manager/ban.js
+++ commands/manager/ban.js
@@ -2,29 +2,84 @@
 
 module.exports.run = async function (client, message, args) {
 	if (message.member.hasPermission(["ADMINISTRATOR", "BAN_MEMBERS"])) {
-		const user = message.mentions.users.first();
-		if (user) {
-			const member = message.guild.member(user);
-			if (member) {
-				member
-					.ban({
-						reason: "พวกเขานิสัยไม่ดีเลยอ่ะ 😢",
-					})
-					.then(() => {
-						message.reply(`แบน ${user.tag} แล้วค่ะ`);
-					})
-					.catch(err => {
-						message.reply("แบนไม่ได้อ๊าาาา...");
-						console.error(err);
+		let arg = args[0];
+		if (arg === undefined) {
+			message.reply("❓ จะให้ฉันจัดการกับสมาชิกคนไหนเหรอคะ")
+				.then(function (msg) {
+					msg.delete({
+						timeout: 10000
+					});
+				});
+		} else {
+			let member = client.users.cache.find(user => (user.username === arg) || (user.id === arg));
+			if (member === undefined) {
+				message.reply("❎ ฉันหาสมาชิกนี้ไม่เจอคะ ลองตรวจสอบใหม่อีกรอบคะ")
+					.then(function (msg) {
+						msg.delete({
+							timeout: 10000
+						});
 					});
 			} else {
-				message.reply("ชื่อไม่คุ้นเลยนะคะ เขียนผิดหรือเปล่าคะ ❔");
+				let memberBan = message.guild.members.cache.get(member.id);
+				if (memberBan === undefined) {
+					message.reply("❎ เอ๋...สมาชิกนี้หายไปไหนแล้วอ่ะ เอ๋...หรือไม่ได้อยู่ที่นี่หรือเปล่า")
+						.then(function (msg) {
+							msg.delete({
+								timeout: 10000
+							});
+						});
+				} else {
+					if (memberBan.banable === false) {
+						message.reply("❌ ฉันไม่มีสิทธิ์ที่จะแบนเขานะคะ ถ้าหากมีเหตุผลใดๆ ที่เขาทำอะไรไม่ดีให้แจ้งเจ้าของที่นี่นะคะ ตอนนี้ฉันทำอะไรเขาไม่ด้ายยย...");
+					} else {
+						let reason = args.slice(1).join(" ");
+						if (reason === "") {
+							reason = "**สมาชิกที่แบนไม่ได้ให้เหตุผลไว้คะ**";
+							ban(member, memberBan, reason);
+						} else {
+							ban(member, memberBan, reason);
+						}
+					}
+				}
 			}
-		} else {
-			message.reply("ขอชื่อสมาชิกด้วยคะ");
 		}
 	} else {
 		return message.reply("🛑 ขอโทษนะคะ แต่ว่าาา...คุณไม่มีสิทธิ์ในการใช้งานฟังก์ชันนี้คะ <:shioru_heavy:694159309877018685>");
+	}
+
+	function ban(member, memberBan, reason) {
+		let notification = message.guild.channels.cache.find(ch => ch.name === "│การแจ้งเตือน🔔");
+
+		let author = message.author.username;
+		let authorAvatar = message.author.displayAvatarURL();
+		let avatar = member.avatarURL();
+		let username = member.username;
+		let time = new Date();
+		memberBan.ban(reason)
+			.then(function () {
+				let embed = {
+					"title": username + " โดนแบน เนื่องจาก:",
+					"description": reason,
+					"color": 16098851,
+					"timestamp": time,
+					"footer": {
+						"icon_url": authorAvatar,
+						"text": "แบนโดย " + author
+					},
+					"thumbnail": {
+						"url": avatar
+					},
+					"author": {
+						"name": "สมาชิก",
+						"icon_url": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/bust-in-silhouette_1f464.png"
+					}
+				};
+				notification.send({
+					embed
+				});
+			}).catch(function (error) {
+				message.reply("⚠️ ฉันทำไม่ได้คะ เพราะว่า: " + error);
+			});
 	}
 };
 
--- commands/manager/kick.js
+++ commands/manager/kick.js
@@ -1,30 +1,85 @@
 // This command is not yet available. //
 
 module.exports.run = async function (client, message, args) {
-		message.delete();
-        if (message.member.hasPermission(["ADMINISTRATOR", "KICK_MEMBERS"])) {
-			const user = message.mentions.users.first();
-			if (user) {
-				const member = message.guild.member(user);
-				if (member) {
-					member.kick("กรุณาระบุเหตุผลเพิ่มเติมที่จะแสดงในบันทึกการตรวจสอบด้วยค่ะ")
-						.then(() => {
-							message.reply(`เตะแล้วค่ะ ⚽ | ลาก่อนนะ ${user.tag}`);
-						})
-						.catch(err => {
-							message.reply("ทำไหมฉันเตะเขาออกไม่ได้ละคะ 😕");
-							console.error(err);
-						});
-				} else {
-					message.reply("เอ๋...ฉันหาสมาชิกนี้ไม่เจอค่ะ เขียนผิดหรือเปล่าคะ ❔");
-				}
-			} else {
-				message.reply("บอกชื่อสมาชิกให้ฉันด้วยจิ...");
-			}
+    if (message.member.hasPermission(["ADMINISTRATOR", "KICK_MEMBERS"])) {
+		let arg = args[0];
+		if (arg === undefined) {
+			message.reply("❓ จะให้ฉันจัดการกับสมาชิกคนไหนเหรอคะ")
+			.then(function (msg) {
+				msg.delete({
+					timeout: 10000
+				});
+			});
 		} else {
-			return message.reply("🛑 ขอโทษนะคะ แต่ว่าาา...คุณไม่มีสิทธิ์ในการใช้งานฟังก์ชันนี้คะ <:shioru_heavy:694159309877018685>");
+			let member = client.users.cache.find(user => (user.username === arg) || (user.id === arg));
+			if (member === undefined) {
+				message.reply("❎ ฉันหาสมาชิกนี้ไม่เจอคะ ลองตรวจสอบใหม่อีกรอบคะ")
+				.then(function (msg) {
+					msg.delete({
+						timeout: 10000
+					});
+				});
+			} else {
+				let memberKick = message.guild.members.cache.get(member.id);
+				if (memberKick === undefined) {
+					message.reply("❎ เอ๋...สมาชิกนี้หายไปไหนแล้วอ่ะ เอ๋...หรือไม่ได้อยู่ที่นี่หรือเปล่า")
+					.then(function (msg) {
+						msg.delete({
+							timeout: 10000
+						});
+					});
+				} else {
+					if (memberKick.kickable === false) {
+						message.reply("❌ ฉันไม่มีสิทธิ์ที่จะนำเขาออกจากที่นี่นะคะ ถ้าหากมีเหตุผลใดๆ ที่เขาทำอะไรไม่ดีให้แจ้งเจ้าของที่นี่นะคะ ตอนนี้ฉันทำอะไรเขาไม่ด้ายยย...");
+					} else {
+						let reason = args.slice(1).join(" ");
+						if (reason === "") {
+							reason = "**สมาชิกที่แตะไม่ได้ให้เหตุผลไว้คะ**";
+							kick(member, memberKick, reason);
+						} else {
+							kick(member, memberKick, reason);
+						}
+					}
+				}
+			}
 		}
-	};
+    } else {
+    	return message.reply("🛑 ขอโทษนะคะ แต่ว่าาา...คุณไม่มีสิทธิ์ในการใช้งานฟังก์ชันนี้คะ <:shioru_heavy:694159309877018685>");
+	}
+	
+	function kick(member, memberKick, reason) {
+		let notification = message.guild.channels.cache.find(ch => ch.name === "│การแจ้งเตือน🔔");
+		
+		let author = message.author.username;
+		let authorAvatar = message.author.displayAvatarURL();
+		let avatar = member.avatarURL();
+		let username = member.username;
+		let time = new Date();
+		memberKick.kick(reason)
+		.then(function () {
+			let embed = {
+				"title": username + " โดนแตะออก เนื่องจาก:",
+				"description": reason,
+				"color": 16098851,
+				"timestamp": time,
+				"footer": {
+					"icon_url": authorAvatar,
+					"text": "แตะโดย " + author
+				},
+				"thumbnail": {
+					"url": avatar
+				},
+				"author": {
+					"name": "สมาชิก",
+					"icon_url": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/bust-in-silhouette_1f464.png"
+				}
+			};
+			notification.send({ embed });
+		}).catch(function (error) {
+			message.reply("⚠️ ฉันทำไม่ได้คะ เพราะว่า: " + error);
+		});
+	}
+};
 	
 module.exports.help = {
 	"name": "kick",
