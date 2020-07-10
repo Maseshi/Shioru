35e62422e300a00cb8efd39d2655b4bb89e0d697
shinosaki <dermhioasw123@gmail.com>
2020-07-10 20:57:12 +0700

    Change new emoji

--- events/client/error.js
+++ events/client/error.js
@@ -1,4 +1,4 @@
 module.exports = error => {
-	message.channel.send("❌ เกิดข้อผิดพลาดภายใน Client: " + error);
+	message.channel.send("⚠️ เกิดข้อผิดพลาดภายใน Client: " + error);
 	console.error(error);
 };
