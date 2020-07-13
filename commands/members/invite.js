8aa1d894cf9f3f91a296c5125309f5984c308afc
shinosaki <dermhioasw123@gmail.com>
2020-07-13 17:41:01 +0700

    Move category

---
+++ commands/members/invite.js
@@ -0,0 +1,26 @@
+module.exports.run = async function (client, message, args) {
+	message.guild.channels.cache.first().createInvite()
+	.then(function (invite) {
+		let embed = {
+			"description": "||" + invite.url + "||",
+			"color": 10197915,
+			"footer": {
+				"icon_url": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/ticket_1f3ab.png",
+				"text": "สินค้านี้ฟรี ห้ามวางจำหนายต่อ"
+			},
+			"author": {
+				"name": "บัตรเชิญสมาชิก",
+				"icon_url": "https://discordapp.com/assets/5e5ab6737c6e2b6a9aa7a1e7295d1b41.svg"
+			}
+		};
+		message.channel.send({ embed });
+	});
+};
+	
+module.exports.help = {
+	"name": "invite",
+	"description": "Create and receive invitation links to join the server.",
+	"usage": "Yinvite",
+	"category": "members",
+	"aliases": ["เชิญ"]
+};
