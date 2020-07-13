42f71b1b4ec86e5746d669b4c4e6d89d80213efe
shinosaki <dermhioasw123@gmail.com>
2020-07-13 21:46:13 +0700

    New commands

---
+++ commands/fun/animTableFlip.js
@@ -0,0 +1,26 @@
+module.exports.run = async function (client, message, args) {
+    let frames = [
+        '(-°□°)-  ┬─┬',
+        '(╯°□°)╯    ]',
+        '(╯°□°)╯  ︵  ┻━┻',
+        '(╯°□°)╯       [',
+        '(╯°□°)╯           ┬─┬'
+    ];
+
+    message.channel.send('(\\\\°□°)\\\\  ┬─┬')
+    .then(function (msg) {
+        for (let frame of frames) {
+            setTimeout(() => {}, 4000);
+            msg.edit(frame);
+        }
+        return;
+    });
+};
+
+module.exports.help = {
+    "name": "animTableFlip",
+    "description": "(\\\\°□°)\\\\  ┬─┬",
+    "usage": "YanimTableFlip",
+    "category": "fun",
+    "aliases": ["animtableflip", "animtf", "atf", "ฟิบโต๊ะ"]
+};
