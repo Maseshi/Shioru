cccb3e5a1acddb3286e6e51dffbfde8be91dd7fe
shinosaki <dermhioasw123@gmail.com>
2020-08-27 20:31:59 +0700

    Test operation .env

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 module.exports = {
     "prefix": "Y",
     "owner": "618836889239158785",
-    "token": process.env.token || TOKEN,
+    "token": process.env.token || secret.TOKEN,
     "firebase": {
-        "apiKey": process.env.apiKey || API_KEY,
-        "databaseURL": process.env.databaseURL || DATABASE_URL,
-        "projectId": process.env.projectId || PROJECT_ID
+        "apiKey": process.env.apiKey || secret.API_KEY,
+        "databaseURL": process.env.databaseURL || secret.DATABASE_URL,
+        "projectId": process.env.projectId || secret.PROJECT_ID
     },
     "update": "2020-06-23T11:03:44.031Z",
-    "password": process.env.password || PASSWORD,
-    "youtubeApi": process.env.youtubeApi || YOUTUBE_API
+    "password": process.env.password || secret.PASSWORD,
+    "youtubeApi": process.env.youtubeApi || secret.YOUTUBE_API
 };
