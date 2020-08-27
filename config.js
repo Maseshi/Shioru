23089ff89dc242ac81588806d86b69f6db5c95fb
shinosaki <dermhioasw123@gmail.com>
2020-08-27 20:27:54 +0700

    Test operation .env

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 module.exports = {
     "prefix": "Y",
     "owner": "618836889239158785",
-    "token": process.env.token || process.jobs.build.steps.env.TOKEN,
+    "token": process.env.token || TOKEN,
     "firebase": {
-        "apiKey": process.env.apiKey || process.jobs.build.steps.env.API_KEY,
-        "databaseURL": process.env.databaseURL || process.jobs.build.steps.env.DATABASE_URL,
-        "projectId": process.env.projectId || process.jobs.build.steps.env.PROJECT_ID
+        "apiKey": process.env.apiKey || API_KEY,
+        "databaseURL": process.env.databaseURL || DATABASE_URL,
+        "projectId": process.env.projectId || PROJECT_ID
     },
     "update": "2020-06-23T11:03:44.031Z",
-    "password": process.env.password || process.jobs.build.steps.env.PASSWORD,
-    "youtubeApi": process.env.youtubeApi || process.jobs.build.steps.env.YOUTUBE_API
+    "password": process.env.password || PASSWORD,
+    "youtubeApi": process.env.youtubeApi || YOUTUBE_API
 };
