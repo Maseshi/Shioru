93e62a671b945f4ef945d8c90c6cd2637924b7eb
shinosaki <dermhioasw123@gmail.com>
2020-08-27 19:39:53 +0700

    Update

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 module.exports = {
     "prefix": "Y",
     "owner": "618836889239158785",
-    "token": process.env.TOKEN,
+    "token": process.env.token,
     "firebase": {
-        "apiKey": process.env.API_KEY,
-        "databaseURL": process.env.DATABASE_URL,
-        "projectId": process.env.PROJECT_ID
+        "apiKey": process.env.apiKey,
+        "databaseURL": process.env.databaseURL,
+        "projectId": process.env.projectId
     },
     "update": "2020-06-23T11:03:44.031Z",
-    "password": process.env.PASSWORD,
-    "youtubeApi": process.env.YOUTUBE_API
+    "password": process.env.password,
+    "youtubeApi": process.env.youtubeApi
 };
