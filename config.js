b03ca63dcab58f9f0abbef6836044e20623de5d1
shinosaki <dermhioasw123@gmail.com>
2020-08-27 21:36:00 +0700

    Test operating environment

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 module.exports = {
     "prefix": "Y",
     "owner": "618836889239158785",
-    "token": process.env.token,
+    "token": process.env.token || $token,
     "firebase": {
-        "apiKey": process.env.apiKey,
-        "databaseURL": process.env.databaseURL,
-        "projectId": process.env.projectId
+        "apiKey": process.env.apiKey || $apiKey,
+        "databaseURL": process.env.databaseURL || $databaseURL,
+        "projectId": process.env.projectId || $projectId
     },
     "update": "2020-06-23T11:03:44.031Z",
-    "password": process.env.password,
-    "youtubeApi": process.env.youtubeApi
+    "password": process.env.password || $password,
+    "youtubeApi": process.env.youtubeApi || $youtubeApi
 };
