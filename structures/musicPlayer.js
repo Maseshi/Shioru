b337564e8b484543d65462ab222e21e5b58cb061
Chaiwat Suwannarat <dermhioasw123@gmail.com>
2020-12-06 21:47:11 +0700

    Increase efficiency and fix problems

--- structures/musicPlayer.js
+++ structures/musicPlayer.js
@@ -1,39 +1,61 @@
-const ytdl = require("ytdl-core-discord");
+const ytdl = require("erit-ytdl");
 
-module.exports = async function (client, message, song) {
+module.exports = async function (client, channel, message, song) {
     let queue = message.client.queue.get(message.guild.id);
 
     if (!song) {
-        message.client.queue.delete(message.guild.id);
+        setTimeout(function () {
+            if (!queue.connection.dispatcher && !message.guild.me.voice.channel) {
+                channel.leave();
+            }
+        }, 500000);
+        queue.textChannel.send("🎐 ตอนนี้คิวเพลงว่างแล้วค้าา...");
+        return message.client.queue.delete(message.guild.id);
     } else {
         queue.connection.on("disconnect", function () {
             message.client.queue.delete(message.guild.id);
         });
 
-        let stream = await ytdl(song.url, { "highWaterMark": 1 << 25 });
-        let type = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
+        let url = song.url;
+        let stream = await ytdl(url, {
+            "highWaterMark": 1 << 25,
+            "opusEncoded": true
+        });
+        let streamType = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
 
         let dispatcher = queue.connection.play(stream, {
-            "type": type,
-            "filter": "audioonly"
+            "type": streamType,
+            "filter": "audioonly",
+            "quality": "highestaudio"
         });
 
-        dispatcher.on('finish', function () {
+        dispatcher.on("end", function () {
             if (queue.loop) {
                 let lastSong = queue.songs.shift();
                 queue.songs.push(lastSong);
-                module.exports(client, message, queue.songs[0]);
+                module.exports(client, channel, message, queue.songs[0]);
             } else {
                 queue.songs.shift();
-                module.exports(client, message, queue.songs[0]);
+                module.exports(client, channel, message, queue.songs[0]);
             }
         });
-        dispatcher.on('error', function (error) {
+        dispatcher.on("finish", function () {
+            if (queue.loop) {
+                let lastSong = queue.songs.shift();
+                queue.songs.push(lastSong);
+                module.exports(client, channel, message, queue.songs[0]);
+            } else {
+                queue.songs.shift();
+                module.exports(client, channel, message, queue.songs[0]);
+            }
+        });
+        dispatcher.on("error", function (error) {
             console.log(error);
-            return message.channel.send("⚠️ เกิดข้อผิดพลาดขณะกำลังเล่นคะ แจ้งมาว่า: " + error);
+            queue.songs.shift();
+            message.channel.send("⚠️ เกิดข้อผิดพลาดขณะกำลังเล่นคะ: " + error);
         });
         dispatcher.setVolumeLogarithmic(queue.volume / 100);
 
-        queue.textChannel.send("🎶 เริ่มเล่นเพลง: `" + song.title + "`");
+        queue.textChannel.send("🎶 กำลังเล่นเพลง: `" + song.title + "`");
     }
 };
