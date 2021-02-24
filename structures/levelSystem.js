ead91b5356208d1fa129c95e878a65f4ad905eb4
Maseshi <dermhioasw123@gmail.com>
2021-02-24 21:51:06 +0700

    Fixed a bug of the database, not creating new files.

--- structures/levelSystem.js
+++ structures/levelSystem.js
@@ -2,18 +2,18 @@ const firebase = require("firebase");
 const messages = require("../events/client/message");
 
 module.exports = async function (client, message) {
+    let guildId = message.guild.id;
     let avatar = message.author.displayAvatarURL();
     let username = message.author.username;
     let id = message.author.id;
 
     let database = firebase.database();
     let ref = database.ref("Shioru/Discord/Users/" + id + "/Leveling/");
+    let dataRef = database.ref("Shioru/Discord/Guilds/").child(guildId);
     ref.once("value").then(function (snapshot) {
         if (snapshot.exists()) {
             let exp = snapshot.val().EXP;
             let level = snapshot.val().Level;
-            let notifyEnable = snapshot.val().channels.notification.enable;
-            let notifyId = snapshot.val().channels.notification.id;
 
             ref.update({
                 "EXP": (exp += 5)
@@ -26,22 +26,44 @@ module.exports = async function (client, message) {
                     "EXP": 0,
                     "Level": ++level
                 }).then(function () {
-                    if (notifyEnable === true) {
-                        let levelUp = message.guild.channels.cache.find(ch => ch.id === notifyId);
+                    dataRef.once("value").then(function (dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            let notifyEnable = snapshot.val().channels.notification.enable;
+                            let notifyId = snapshot.val().channels.notification.id;
 
-                        let embed = {
-                            "description": client.lang.event_client_message_embed_description.replace('%username', username).replace('%level', level),
-                            "color": 16312092,
-                            "thumbnail": {
-                                "url": avatar
-                            },
-                            "author": {
-                                "name": client.lang.event_client_message_embed_author_name,
-                                "icon_url": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/confetti-ball_1f38a.png"
+                            if (notifyEnable === true) {
+                                let levelUp = message.guild.channels.cache.find(ch => ch.id === notifyId);
+        
+                                let embed = {
+                                    "description": client.lang.event_client_message_embed_description.replace('%username', username).replace('%level', level),
+                                    "color": 16312092,
+                                    "thumbnail": {
+                                        "url": avatar
+                                    },
+                                    "author": {
+                                        "name": client.lang.event_client_message_embed_author_name,
+                                        "icon_url": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/confetti-ball_1f38a.png"
+                                    }
+                                };
+                                levelUp.send({ embed });
                             }
-                        };
-                        levelUp.send({ embed });
-                    }
+                        } else {
+                            ref.set({
+                                "prefix": "S",
+                                "language": "th_TH",
+                                "channels": {
+                                    "notification": {
+                                        "enable": false,
+                                        "id": 0
+                                    }
+                                }
+                            }).then(function () {
+                                messages(client, message);
+                            }).catch(function (error) {
+                                console.log(error);
+                            });
+                        }
+                    });
                 }).catch(function (error) {
                     console.log(error);
                 });
