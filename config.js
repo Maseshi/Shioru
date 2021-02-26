1ea2d7e839e99116294a55042920b5ed535dc75b
Maseshi <dermhioasw123@gmail.com>
2021-02-26 16:16:54 +0700

    Fixed the issue of Node.js CI.

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ module.exports = {
         "ชิโอรุ"
     ],
     "owner": "618836889239158785",
-    "token": process.env.token || $TOKEN,
+    "token": process.env.token || $token,
     "firebase": {
-        "apiKey": process.env.apiKey || $APIKEY,
-        "databaseURL": process.env.databaseURL || $DATABASEURL,
-        "projectId": process.env.projectId || $PROJECTID
+        "apiKey": process.env.apiKey || $apiKey,
+        "databaseURL": process.env.databaseURL || $databaseURL,
+        "projectId": process.env.projectId || $projectId
     },
     "update": "2021-01-01T18:57:20.432Z",
-    "password": process.env.password || $PASSWORD,
-    "youtubeApi": process.env.youtubeApi || $YOUTUBEAPI
+    "password": process.env.password || $password,
+    "youtubeApi": process.env.youtubeApi || $youtubeApi
 };
