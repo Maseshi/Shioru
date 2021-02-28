774a1c5c56489d43a62a362978126411daa21f36
Maseshi <dermhioasw123@gmail.com>
2021-02-28 18:38:16 +0700

    Modify the content

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const config = require("./config");
 const lang = require("./languages/th_TH.json");
 
 // Show when bots start working To check that bots do not have a problem
-console.log("\x1b[31m======================================");
-console.log("\x1b[33m(c) 2021 Masashi. All rights reserved.");
-console.group("\x1b[31m======================================\x1b[0m");
+console.log("\x1b[31m================================================================");
+console.log("\x1b[33mCopyright (c) 2020-2021 Chaiwat Suwannarat. All rights reserved.");
+console.group("\x1b[31m================================================================\x1b[0m");
 
 const client = new discord.Client({
     "autoReconnect": true,
@@ -17,14 +17,14 @@ const client = new discord.Client({
 
 client.config = config;
 client.lang = lang;
-client.queue = new Map();
+client.data = new Map();
 
 ["command", "event"].forEach(dirs => require("./handlers/" + dirs)(client));
 
 // Check package version
-console.log("\u001b[1mpackage\u001b[0m version: " + packageInfo.version);
-console.log("\u001b[1mdiscord.js\u001b[0m version: " + discord.version);
-console.log("\u001b[1mnode.js\u001b[0m version: " + process.version);
+console.log("\u001b[1mPackage\u001b[0m version: " + packageInfo.version);
+console.log("\u001b[1mDiscord.js\u001b[0m version: " + discord.version);
+console.log("\u001b[1mNode.js\u001b[0m version: " + process.version);
 
 firebase.initializeApp(client.config.firebase);
 client.login(client.config.token);
