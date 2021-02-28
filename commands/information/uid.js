44d091454f404ccf91144ffad4ac376e8ad37561
Maseshi <dermhioasw123@gmail.com>
2021-02-28 18:44:13 +0700

    Rename the command to uid.

---
+++ commands/information/uid.js
@@ -0,0 +1,40 @@
+module.exports.run = async function (client, message, args) {
+    let avatar = message.author.displayAvatarURL();
+    let username = message.author.username;
+    let id = message.author.id;
+    let arg = args.join(" ");
+    if (arg) {
+        let user = client.users.cache.find(users => (users.username === arg) || (users.id === arg) || (users.tag === arg));
+        if (!user) {
+            message.channel.send(client.lang.command_information_id_no_args);
+        } else {
+            avatar = user.avatarURL();
+            username = user.username;
+            id = user.id;
+            userID(avatar, username, id);
+        }
+    } else {
+        userID(avatar, username, id);
+    }
+
+    function userID(Savatar, Susername, Sid) {
+        message.channel.send({
+            "embed": {
+                "title": client.lang.command_information_id_function_userID_embed_title + Susername,
+                "description": "```" + Sid + "```" + client.lang.command_information_id_function_userID_embed_description,
+                "color": 4886754,
+                "thumbnail": {
+                    "url": Savatar
+                }
+            }
+        });
+    }
+};
+
+module.exports.help = {
+    "name": "uid",
+    "description": "Get your user id",
+    "usage": "uid (Member)",
+    "category": "information",
+    "aliases": ["myid", "myId", "UID", "รหัส", "ไอดี", "รหัสบัญชี"]
+};
