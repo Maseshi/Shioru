1ebce519fdf6a93576e201b4e70c302c4bea94a8
Maseshi <dermhioasw123@gmail.com>
2021-05-16 23:47:46 +0700

    Add permissions and fix common problems.

--- events/client/message.js
+++ events/client/message.js
@@ -3,59 +3,46 @@ const levelSystem = require("../../structures/levelSystem");
 const settingsData = require("../../structures/settingsData");
 
 module.exports = function (client, message) {
+    settingsData(client, message);
+    levelSystem(client, message);
+
     let prefix = client.config.prefix;
-    let methods = client.config.method;
-    let method = false;
+    let mentions = client.config.mention;
+    let mention = false;
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
-    let mhs = message.content.slice(method.length).trim().split(/ +/g);
+    let mts = message.content.slice(mention.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
     let command;
 
-    if (message.author.bot) {
-        return;
-    } else {
-        if (message.channel.type === "dm") {
-            return;
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+    
+    for (let thisMention of mentions) {
+        if (message.content.startsWith(thisMention)) {
+            mention = thisMention;
+            if (mention.length === 0) return;
+            mts.shift();
+            if (mention) answer(client, message, args, mts);
+        }
+    }
+
+    if (message.content.startsWith(prefix)) {
+        if (cmd.length === 0) return;
+        if (client.commands.has(cmd)) {
+            command = client.commands.get(cmd);
+        } else if (client.aliases.has(cmd)) {
+            command = client.commands.get(client.aliases.get(cmd));
+        }
+
+        if (command) {
+            if (command.help.permissions) {
+                if (!message.member.hasPermission(command.help.permissions)) {
+                    return message.reply(client.lang.event_client_message_permissions);
+                }
+            }
+            command.run(client, message, args);
         } else {
-            settingsData(client, message);
-            levelSystem(client, message);
-
-            // For talking
-            for (const thisMethod of methods) {
-                if (message.content.startsWith(thisMethod)) {
-                    method = thisMethod;
-                    if (method.length === 0) {
-                        return;
-                    } else {
-                        mhs.shift();
-
-                        if (method) {
-                            answer(client, message, args, mhs);
-                        }
-                    }
-                }
-            }
-
-            // For commands
-            if (message.content.startsWith(prefix)) {
-                if (cmd.length === 0) {
-                    return;
-                } else {
-                    if (client.commands.has(cmd)) {
-                        command = client.commands.get(cmd);
-                    } else if (client.aliases.has(cmd)) {
-                        command = client.commands.get(client.aliases.get(cmd));
-                    }
-
-                    if (command) {
-                        command.run(client, message, args);
-                    } else {
-                        if (!method) {
-                            console.log("\u001b[4m" + message.author.username + "\u001b[0m Type an unknown command: \u001b[34m" + cmd + "\u001b[0m");
-                        }
-                    }
-                }
-            }
+            if (!mention) console.log("\u001b[4m" + message.author.username + "\u001b[0m Type an unknown command: \u001b[34m" + cmd + "\u001b[0m");
         }
     }
 };
