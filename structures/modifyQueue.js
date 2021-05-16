c2387524309bdc487e33eb6a913054eff6c62754
Maseshi <dermhioasw123@gmail.com>
2021-05-16 23:46:30 +0700

    Not necessary

--- structures/modifyQueue.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (member) {
-    let memberChannel = member.voice.channelID;
-    let clientChannel = member.guild.voice.channelID;
-
-    if (memberChannel !== clientChannel) return;
-    
-    return true;
-};
