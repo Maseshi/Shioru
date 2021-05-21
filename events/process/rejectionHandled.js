fd1a82b53eaf22498e4c2bf58f0cf4148be7bfe9
Maseshi <dermhioasw123@gmail.com>
2021-05-22 00:32:39 +0700

    These commands did not work and were moved to the main operation.

--- events/process/rejectionHandled.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (error) {
-    console.error("Rejection promise rejection:", error);
-};
--- events/process/unhandledRejection.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (error) {
-    console.error("Unhandled promise rejection:", error);
-};
