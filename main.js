f114ccc0491991be8f74b515a44b01c05cdc5ed1
Maseshi <dermhioasw123@gmail.com>
2021-05-23 16:36:30 +0700

    Reconfigure the client.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const discord = require("discord.js");
 const firebase = require("firebase");
 const packageInfo = require("./package.json");
-const config = require("./config");
+const clientConfig = require("./config/client");
+const dataConfig = require("./config/data");
 const lang = require("./languages/th_TH.json");
 
 // Show when bots start working To check that bots do not have a problem
@@ -9,22 +10,31 @@ console.log("\x1b[31m┏━━━━━━━━━━━━━━━━━━�
 console.log("\x1b[31m┃\x1b[33m Copyright (c) 2020-2021 Chaiwat Suwannarat. All rights reserved. \x1b[31m┃");
 console.group("\x1b[31m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\x1b[0m");
 
+// Check package version
+console.log("\u001b[1mPackage\u001b[0m version: " + packageInfo.version);
+console.log("\u001b[1mDiscord.js\u001b[0m version: " + discord.version);
+console.log("\u001b[1mNode.js\u001b[0m version: " + process.version);
+
 const client = new discord.Client({
+    "messageCacheLifetime": 60,
+    "fetchAllMembers": false,
+    "messageCacheMaxSize": 10,
+    "restTimeOffset": 0,
+    "restWsBridgetimeout": 100,
     "autoReconnect": true,
     "disableEveryone": false,
     "partials": ["MESSAGE", "CHANNEL", "REACTION"]
 });
 
-client.config = config;
-client.lang = lang;
-client.data = new Map();
+client.data = {
+    "config": {
+        "client": clientConfig,
+        "data": dataConfig
+    },
+    "language": lang
+};
 
-["command", "event"].forEach(dirs => require("./handlers/" + dirs)(client));
-
-// Check package version
-console.log("\u001b[1mPackage\u001b[0m version: " + packageInfo.version);
-console.log("\u001b[1mDiscord.js\u001b[0m version: " + discord.version);
-console.log("\u001b[1mNode.js\u001b[0m version: " + process.version);
+["command", "event", "music"].forEach(dirs => require("./handlers/" + dirs)(client));
 
 process.on("rejectionHandled", function (error) {
     console.error("Rejection promise rejection:", error);
@@ -38,5 +48,5 @@ process.on("unhandledRejection", function (error) {
     console.error("Unhandled promise rejection:", error);
 });
 
-firebase.initializeApp(client.config.firebase);
-client.login(client.config.token);
+firebase.initializeApp(client.data.config.data.server);
+client.login(client.data.config.client.token);
