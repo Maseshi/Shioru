80a2a63e5a978ce7ccb3baecfc94590c8790a725
Maseshi <dermhioasw123@gmail.com>
2022-04-29 22:49:19 +0700

    Update the core system of the bot.

--- source/main.js
+++ source/main.js
@@ -4,7 +4,9 @@ const { SpotifyPlugin } = require("@distube/spotify");
 const { SoundCloudPlugin } = require("@distube/soundcloud");
 const { YtDlpPlugin } = require("@distube/yt-dlp")
 const { initializeApp } = require("firebase/app");
-const { readdirSync } = require("fs");
+const { readdirSync, readFileSync } = require("node:fs");
+const { version } = require("../package.json")
+const ansiColor = require("./extras/ansiColor");
 const config = require("./config/data");
 const language = require("./languages/en.json");
 
@@ -12,23 +14,44 @@ const language = require("./languages/en.json");
 console.time("\u001b[34m\u001b[7m Bot is ready to work on the servers! \u001b[0m");
 
 // Show when bots start working To check that bots do not have a problem
-if (config.mode === "production") {
-    console.log("\u001b[31;1m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
-    console.log("\u001b[31;1m┃\u001b[33;1m Copyright (c) 2020-2022 Maseshi. All rights reserved. \u001b[31;1m┃");
-    console.group("\u001b[31;1m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\u001b[0m");
-}
-if (config.mode === "development") {
-    console.log("\u001b[31;1m┏━━━━━━━━━━━━━━━━━━━ DEVELOPMENT MODE ━━━━━━━━━━━━━━━━━━┓");
-    console.log("\u001b[31;1m┃\u001b[33;1m Copyright (c) 2020-2022 Maseshi. All rights reserved. \u001b[31;1m┃");
-    console.group("\x1b[31m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\u001b[0m");
+const clearStyle = ansiColor(0, "sgr");
+const underlineStyle = ansiColor(4, "sgr");
+const whiteColor = ansiColor(15, "foreground");
+const yellowColor = ansiColor(11, "foreground");
+const blueBrightColor = ansiColor(33, "foreground");
+
+const file = readFileSync("./source/assets/ascii-art.txt", "utf-8");
+const text = file.split('\n').join("\n");
+const textFormat =  text.replace("%s1", "v")
+                        .replace("%s2", version.charAt(0))
+                        .replace("%s3", version.charAt(1))
+                        .replace("%s4", version.charAt(2))
+                        .replace("%s5", version.charAt(3))
+                        .replace("%s6", version.charAt(4));
+
+console.clear();
+console.info(blueBrightColor + textFormat + clearStyle);
+console.info(whiteColor + "Copyright (C) 2020-2022 Chaiwat Suwannarat. All rights reserved." + clearStyle);
+console.info(whiteColor + "Website: https://shiorus.web.app/\n" + clearStyle);
+
+// Check the work system from the script in packages.json.
+// Example of use in development mode: npm run dev
+// Production mode is "start"
+// Development mode is "dev"
+if (process.env.npm_lifecycle_event && process.env.npm_lifecycle_event === "dev") {
+    console.info(yellowColor + "┏━━━━━━━━━━━━━━ DEVELOPMENT MODE ━━━━━━━━━━━━━━┓" + clearStyle);
+    console.info(yellowColor + "┃                                              ┃" + clearStyle);
+    console.info(yellowColor + "┃  When in development mode some features may  ┃" + clearStyle);
+    console.info(yellowColor + "┃     not work, you can restart your system    ┃" + clearStyle);
+    console.info(yellowColor + "┃   immediately by typing " + underlineStyle + "rs" + clearStyle + yellowColor + " on the terminal.  ┃" + clearStyle);
+    console.info(yellowColor + "┃                                              ┃" + clearStyle);
+    console.info(yellowColor + "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n" + clearStyle);
 }
 
 // Client setup
 const client = new Client({
-    // "allowedMentions" for using "message.reply"
-    "allowedMentions": {
-        "repliedUser": true
-    },
+    // Fetch the recommended amount of shards from Discord and spawn that amount
+    "shards": "auto",
     // Preset status
     "presence": {
         "status": "dnd",
@@ -56,12 +79,14 @@ const client = new Client({
         "GUILD_MESSAGE_TYPING",
         "DIRECT_MESSAGES",
         "DIRECT_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGE_TYPING"
+        "DIRECT_MESSAGE_TYPING",
+        "GUILD_SCHEDULED_EVENTS"
     ]
 });
 
 // Configure in client
 client.temp = {};
+client.mode = process.env.npm_lifecycle_event || "start";
 client.config = config;
 client.translate = language;
 client.music = new DisTube(client, {
