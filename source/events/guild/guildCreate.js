7e9981b83e732189841b61cc28c45c464860e32d
Maseshi <dermhioasw123@gmail.com>
2022-04-29 23:31:39 +0700

    Remove duplicate submissions to the database.

--- source/events/guild/guildCreate.js
+++ source/events/guild/guildCreate.js
@@ -1,52 +1,23 @@
-const { getDatabase, ref, child, get, set } = require("firebase/database");
 const { MessageAttachment } = require("discord.js");
 const settingsData = require("../../extras/settingsData");
 const catchError = require("../../extras/catchError");
 
-module.exports = async (client, guild) => {
-    if (client.config.mode === "production") {
+module.exports = async (guild) => {
+    const client = guild.client;
+
+    if (client.mode === "start") {
         settingsData(client, guild, module.exports);
         if (client.config.worker !== 1) return;
     }
 
-    let lang;
     const guildName = guild.name;
+    const languageCode = client.config.lang.code;
     const clientAvatar = client.user.displayAvatarURL();
     const clientUsername = client.user.username;
 
-    const db = getDatabase();
-	const childRef = child(ref(db, "Shioru/apps/discord/guilds"), guild.id);
-    const snapshot = await get(child(childRef, "config"));
-
-    if (snapshot.exists()) {
-        const prefix = snapshot.val().prefix;
-        lang = snapshot.val().language;
-
-        client.config.prefix = prefix;
-        client.config.lang.code = lang;
-        client.translate = require("../../languages/" + lang + ".json");
-    } else {
-        set(child(childRef, "config"), {
-            "prefix": "S",
-            "language": "en",
-            "notification": {
-                "alert": 0,
-                "channelCreate": 0,
-                "channelDelete": 0,
-                "channelPinsUpdate": 0,
-                "channelUpdate": 0,
-                "emojiCreate": 0,
-                "emojiDelete": 0,
-                "emojiUpdate": 0,
-                "guildMemberAdd": 0,
-                "guildMemberRemove": 0
-            }
-        });
-    }
-
     guild.channels.cache.forEach((channel) => {
         if (channel.type === "GUILD_TEXT" && channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-            const attachment = new MessageAttachment("./source/assets/images/shioru-discord-cover-" + lang + ".png", "shioru-discord-cover.png");
+            const attachment = new MessageAttachment("./source/assets/images/shioru-discord-cover-" + languageCode + ".png", "shioru-discord-cover.png");
 
             channel.send({
                 "embeds": [
@@ -82,7 +53,7 @@ module.exports = async (client, guild) => {
                         ]
                     }
                 ],
-                "files": [ attachment ]
+                "files": [attachment]
             }).catch((error) => {
                 catchError(client, guild.systemChannel, "guildCreate", error);
             });
