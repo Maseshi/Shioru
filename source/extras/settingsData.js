cda52220d9ea7ade6e6ca0a1b8ca19735e781878
Maseshi <dermhioasw123@gmail.com>
2022-04-29 23:01:25 +0700

    Fixed a bug about each event.

--- source/extras/settingsData.js
+++ source/extras/settingsData.js
@@ -1,9 +1,8 @@
 const { getDatabase, ref, child, onValue, set } = require("firebase/database");
 const catchError = require("./catchError");
 
-module.exports = (client, data, exports) => {
-    const db = getDatabase();
-    const childRef = child(ref(db, "Shioru/apps/discord/guilds"), (data.guild.id || data.id));
+module.exports = (client, guild, exports) => {
+    const childRef = child(ref(getDatabase(), "Shioru/apps/discord/guilds"), guild.id);
 
     onValue(child(childRef, "config"), (snapshot) => {
         if (snapshot.exists()) {
@@ -13,30 +12,30 @@ module.exports = (client, data, exports) => {
             client.config.prefix = prefix;
             client.config.lang.code = lang;
             client.translate = require("../languages/" + lang + ".json");
-            if (!client.config.worker) {
-                client.config.worker = 1;
-                return exports(client, data);
+            if (!client.temp.set) {
+                client.temp.set = 1;
+                return exports(client, guild);
             }
         } else {
             set(child(childRef, "config"), {
                 "prefix": "S",
                 "language": "en",
                 "notification": {
-                    "alert": 0,
-                    "channelCreate": 0,
-                    "channelDelete": 0,
-                    "channelPinsUpdate": 0,
-                    "channelUpdate": 0,
-                    "emojiCreate": 0,
-                    "emojiDelete": 0,
-                    "emojiUpdate": 0,
-                    "guildMemberAdd": 0,
-                    "guildMemberRemove": 0
+                    "alert": false,
+                    "channelCreate": false,
+                    "channelDelete": false,
+                    "channelPinsUpdate": false,
+                    "channelUpdate": false,
+                    "emojiCreate": false,
+                    "emojiDelete": false,
+                    "emojiUpdate": false,
+                    "guildMemberAdd": false,
+                    "guildMemberRemove": false
                 }
             }).then(() => {
                 module.exports(client, data);
             }).catch((error) => {
-                catchError(client, data, "settingsData", error);
+                catchError(client, guild, "settingsData", error);
             });
         }
     });
