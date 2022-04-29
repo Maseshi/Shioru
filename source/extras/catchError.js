8f477c48792e8825242929a8dea695b5c73c73b6
Maseshi <dermhioasw123@gmail.com>
2022-04-29 23:10:05 +0700

    Added catch error for interactions.

--- source/extras/catchError.js
+++ source/extras/catchError.js
@@ -1,9 +1,12 @@
 const { getApps } = require("firebase/app");
 const discord = require("discord.js");
 const packages = require("../../package.json");
+const logGenerator = require("./logGenerator")
+const ansiColor = require("./ansiColor")
 
-module.exports = (client, message, command, error) => {
-    if (!error) return;
+module.exports = async (client, message, name, error) => {
+    if (!name) return console.log("[catchError] Please specify the name of the command or function.");
+    if (!error) return console.log("[catchError] Please forward any errors that have occurred.");
 
     const dateTime = (date) => {
         const day = date.getDay();
@@ -19,20 +22,20 @@ module.exports = (client, message, command, error) => {
         const ping = Date.now() - message.createdTimestamp;
         const api = Math.round(client.ws.ping);
 
-        if ((message && message.author && message.author.id) === client.user.id) {
+        if ((message.author && message.author.id) === client.user.id) {
             message.edit({
                 "content": null,
                 "embeds": [
                     {
                         "title": client.translate.extras.catchError.an_error_occurred,
                         "description": client.translate.extras.catchError.error_detail
-                        .replace("%s1", command)
-                        .replace("%s2", packages.version)
-                        .replace("%s3", new Date())
-                        .replace("%s4", ((getApps().length === 0) ? client.translate.extras.catchError.server_abnormal : client.translate.extras.catchError.server_normal))
-                        .replace("%s5", ping)
-                        .replace("%s6", api)
-                        .replace("%s7", error),
+                            .replace("%s1", name)
+                            .replace("%s2", packages.version)
+                            .replace("%s3", new Date())
+                            .replace("%s4", ((getApps().length === 0) ? client.translate.extras.catchError.server_abnormal : client.translate.extras.catchError.server_normal))
+                            .replace("%s5", ping)
+                            .replace("%s6", api)
+                            .replace("%s7", error),
                         "color": 13632027,
                         "timestamp": new Date()
                     }
@@ -46,47 +49,75 @@ module.exports = (client, message, command, error) => {
                         {
                             "title": client.translate.extras.catchError.an_error_occurred,
                             "description": client.translate.extras.catchError.error_detail
-                            .replace("%s1", command)
-                            .replace("%s2", packages.version)
-                            .replace("%s3", new Date())
-                            .replace("%s4", ((getApps().length === 0) ? client.translate.extras.catchError.server_abnormal : client.translate.extras.catchError.server_normal))
-                            .replace("%s5", ping)
-                            .replace("%s6", api)
-                            .replace("%s7", error),
+                                .replace("%s1", name)
+                                .replace("%s2", packages.version)
+                                .replace("%s3", new Date())
+                                .replace("%s4", ((getApps().length === 0) ? client.translate.extras.catchError.server_abnormal : client.translate.extras.catchError.server_normal))
+                                .replace("%s5", ping)
+                                .replace("%s6", api)
+                                .replace("%s7", error),
                             "color": 13632027,
                             "timestamp": new Date()
                         }
                     ]
                 });
             } catch {
-                message.send({
-                    "content": null,
-                    "embeds": [
-                        {
-                            "title": client.translate.extras.catchError.an_error_occurred,
-                            "description": client.translate.extras.catchError.error_detail
-                            .replace("%s1", command)
-                            .replace("%s2", packages.version)
-                            .replace("%s3", new Date())
-                            .replace("%s4", ((getApps().length === 0) ? client.translate.extras.catchError.server_abnormal : client.translate.extras.catchError.server_normal))
-                            .replace("%s5", ping)
-                            .replace("%s6", api)
-                            .replace("%s7", error),
-                            "color": 13632027,
-                            "timestamp": new Date()
-                        }
-                    ]
-                });
+                try {
+                    message.send({
+                        "content": null,
+                        "embeds": [
+                            {
+                                "title": client.translate.extras.catchError.an_error_occurred,
+                                "description": client.translate.extras.catchError.error_detail
+                                    .replace("%s1", name)
+                                    .replace("%s2", packages.version)
+                                    .replace("%s3", new Date())
+                                    .replace("%s4", ((getApps().length === 0) ? client.translate.extras.catchError.server_abnormal : client.translate.extras.catchError.server_normal))
+                                    .replace("%s5", ping)
+                                    .replace("%s6", api)
+                                    .replace("%s7", error),
+                                "color": 13632027,
+                                "timestamp": new Date()
+                            }
+                        ]
+                    });
+                } catch {
+                    await message.editReply({
+                        "content": null,
+                        "embeds": [
+                            {
+                                "title": client.translate.extras.catchError.an_error_occurred,
+                                "description": client.translate.extras.catchError.error_detail
+                                    .replace("%s1", name)
+                                    .replace("%s2", packages.version)
+                                    .replace("%s3", new Date())
+                                    .replace("%s4", ((getApps().length === 0) ? client.translate.extras.catchError.server_abnormal : client.translate.extras.catchError.server_normal))
+                                    .replace("%s5", ping)
+                                    .replace("%s6", api)
+                                    .replace("%s7", error),
+                                "color": 13632027,
+                                "timestamp": new Date()
+                            }
+                        ]
+                    });
+                }
             }
         }
     }
 
-    console.group(dateTime(new Date()) + " :: \u001b[41;1mUnhandled Rejection/Catch\u001b[0m");
-        console.group("\u001b[1mFull Error:\u001b[0m ");
+    const clearStyle = ansiColor(0, "sgr");
+    const boldStyle = ansiColor(1, "sgr");
+    const whiteColor = ansiColor(15, "foreground");
+    const redBackground = ansiColor(9, "background");
+
+    logGenerator("catch", error)
+
+    console.group(dateTime(new Date()) + " :: " + redBackground + whiteColor + boldStyle + "Catch Error" + clearStyle);
+        console.group(boldStyle + "Full Error:" + clearStyle);
             console.error(error);
         console.groupEnd();
-        console.info("\u001b[1mPackage:\u001b[0m v" + packages.version);
-        console.info("\u001b[1mDiscord.js:\u001b[0m v" + discord.version);
-        console.info("\u001b[1mNode.js:\u001b[0m " + process.version);
+        console.info(boldStyle + "Package:" + clearStyle + " v" + packages.version);
+        console.info(boldStyle + "Discord.js:" + clearStyle + " v" + discord.version);
+        console.info(boldStyle + "Node.js: " + clearStyle + process.version);
     console.groupEnd();
 };
