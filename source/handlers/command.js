d200516a660a6877bd0258583047d6b25fc599a9
Maseshi <dermhioasw123@gmail.com>
2023-03-01 22:28:27 +0700

    🗳️ Change the load of all commands.

--- source/handlers/command.js
+++ source/handlers/command.js
@@ -1,16 +1,12 @@
 const { Collection } = require("discord.js");
 const { readdirSync } = require("node:fs");
-const { ansiColor } = require("../utils/consoleUtils");
 
 module.exports = (client) => {
-  const clearStyle = ansiColor(0, "sgr");
-  const GrayStyle = ansiColor(8, "foreground");
-
   client.console.add("commands-loading", {
     "text": "All commands are starting to load."
   });
 
-  ["commands", "aliases"].forEach(dirs => client[dirs] = new Collection());
+  ["commands"].forEach(dirs => client[dirs] = new Collection());
 
   readdirSync("./source/commands/").forEach((dirs) => {
     const commands = readdirSync("./source/commands/" + dirs + "/").filter(files => files.endsWith(".js"));
@@ -18,58 +14,98 @@ module.exports = (client) => {
     for (const file of commands) {
       const pull = require("../commands/" + dirs + "/" + file);
 
-      if (pull.command) {
-        if (pull.command.enable) {
-          if (typeof pull.name === "string" && typeof pull.description === "string" && typeof pull.category === "string") {
-            if (client.commands.get(pull.name)) {
-              client.console.fail("commands-loading", {
-                "text": "Two or more commands have the same name " + pull.name,
-                "failColor": "yellowBright"
-              });
-              process.exit(0);
-            } else {
-              client.console.update("commands-loading", {
-                "text": "Loading command " + pull.name + " in category " + pull.category
-              });
-
-              client.commands.set(pull.name, pull);
-            }
-          } else {
-            client.console.fail("commands-loading", {
-              "text": "Error loading command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing name or name is not a string. or you have a missing description or description is not a string. or you have a missing category or category is not a string.",
-            });
-            process.exit(1);
-          }
-
-          if (pull.command.aliases && typeof (pull.command.aliases) === "object") {
-            pull.command.aliases.forEach((alias) => {
-              if (client.aliases.get(alias)) {
-                client.console.fail("commands-loading", {
-                  "text": "Two commands or more commands have the same aliases " + alias,
-                  "failColor": "yellowBright"
-                });
-                process.exit(0);
-              } else {
-                client.aliases.set(alias, pull.name);
-              }
-            });
-          }
-        } else {
-          console.group();
-            console.warn(GrayStyle + pull.name + " command is now disabled by default." + clearStyle);
-          console.groupEnd();
-        }
-      } else {
+      // Check the required information.
+      if (typeof pull.enable !== "boolean") {
         client.console.fail("commands-loading", {
-          "text": "Error loading command in " + ("./source/commands/" + dirs + "/" + file) + ". The command hasn't been defined yet.",
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing enable or enable is not a boolean.",
+        });
+        return process.exit(1);
+      }
+      if (typeof pull.name !== "string") {
+        client.console.fail("commands-loading", {
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing name or name is not a string.",
+        });
+        return process.exit(1);
+      }
+      if (typeof pull.description !== "string") {
+        client.console.fail("commands-loading", {
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing description or description is not a string.",
+        });
+        return process.exit(1);
+      }
+      if (typeof pull.category !== "string") {
+        client.console.fail("commands-loading", {
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing category or category is not a string.",
+        });
+        return process.exit(1);
+      }
+
+      // Check the required functions.
+      if (typeof pull.function.command.data !== "object") {
+        client.console.fail("commands-loading", {
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing data or data is not a object.",
+        });
+        return process.exit(1);
+      }
+      if (typeof pull.function.command.data.name !== "string") {
+        client.console.fail("commands-loading", {
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing data.name or data.name is not a string."
+        });
+        return process.exit(1);
+      }
+      if (typeof pull.function.command.data.description !== "string") {
+        client.console.fail("commands-loading", {
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing data.description or data.description is not a string"
+        });
+        return process.exit(1);
+      }
+      if (typeof pull.function.command.execute !== "function") {
+        client.console.fail("commands-loading", {
+          "text": "Error loading application (/) command in " + ("./source/commands/" + dirs + "/" + file) + ". command could not be found or it may not be a function.",
+          "failColor": "yellowBright"
+        });
+        return process.exit(0);
+      }
+      if (pull.function.context) {
+        if (typeof pull.function.context.data !== "object") {
+          client.console.fail("commands-loading", {
+            "text": "Error loading application (/) context in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing data or data is not a object.",
+          });
+          return process.exit(1);
+        }
+        if (typeof pull.function.context.data.name !== "string") {
+          client.console.fail("commands-loading", {
+            "text": "Error loading application context in " + ("./source/commands/" + dirs + "/" + file) + ". you have a missing data.name or data.name is not a string."
+          });
+          return process.exit(1);
+        }
+        if (typeof pull.function.context.execute !== "function") {
+          client.console.fail("commands-loading", {
+            "text": "Error loading application context in " + ("./source/commands/" + dirs + "/" + file) + ". command could not be found or it may not be a function.",
+            "failColor": "yellowBright"
+          });
+          return process.exit(0);
+        }
+      }
+
+      // import all commands
+      if (client.commands.get(pull.name)) {
+        client.console.fail("commands-loading", {
+          "text": "Two or more commands have the same name " + pull.name,
           "failColor": "yellowBright"
         });
         process.exit(0);
+      } else {
+        client.console.update("commands-loading", {
+          "text": "Loading command " + pull.name + " in category " + pull.category
+        });
+
+        client.commands.set(pull.name, pull);
       }
     }
   });
 
   client.console.succeed("commands-loading", {
-    "text": "All commands are verified. Did not find any problems."
+    "text": "All application commands are verified. Did not find any problems."
   });
 };
