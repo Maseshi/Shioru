28842237ef033766f93ac9e85335e3131e009992
Maseshi <dermhioasw123@gmail.com>
2023-05-25 13:49:14 +0700

    💿 Update script for startup

--- start.bat
+++ start.bat
@@ -1,9 +1,55 @@
 @echo off
 
 cls
-echo Start with start.bat...
+net session >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 powershell start -verb runas "%0" am_admin & exit /b
+
+echo Start with start.bat in administrator mode...
+
+echo Verifying installation of Chocolatey...
+call :VerifyChocolatey
+
+echo Initiate component upgrade...
+call choco upgrade all -y
+echo The component upgrade is complete.
+
+echo Verifying installation of FFmpeg...
+call :VerifyFFmpeg
+
+echo Installing build components...
+call choco install build-essential -y
+call choco install autoconf automake g++ libtool -y
+echo Finish installing building components.
+
 echo Installing components...
-npm install --save
+call npm install --save
 echo Completing the component installation.
+
 echo Starting the system...
-node ./source/shard.js
+call node ./source/shard.js
+exit /b 0
+
+:VerifyChocolatey
+call choco --version
+
+if %ERRORLEVEL% EQU 0 exit /b 1
+
+echo Installing Chocolatey...
+powershell Set-ExecutionPolicy RemoteSigned
+powershell iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+echo Updating Chocolatey...
+call choco upgrade chocolatey -y
+echo Finish installing and updating Chocolatey.
+exit /b 1
+
+:VerifyFFmpeg
+call ffmpeg -version
+
+if %ERRORLEVEL% EQU 0 exit /b 1
+
+echo Installing FFmpeg...
+call choco install ffmpeg -y
+echo Updating FFmpeg...
+call choco upgrade ffmpeg -y
+echo Finish installing and updating FFmpeg.
+exit /b 1
--- start.sh
+++ start.sh
@@ -1,12 +1,25 @@
 #!/bin/sh -x
 
 clear
+
 echo Start with start.sh in administrator mode....
+
 echo Initiate component update...
-sudo apt-get update
+apt-get update -y || sudo apt-get update -y
 echo The component update is complete.
+
+echo Installing FFmpeg...
+apt-get install ffmpeg -y || sudo apt-get install ffmpeg -y
+echo Finished installing FFmpeg.
+
+echo Installing build components...
+apt-get install build-essential -y || sudo apt-get install build-essential -y
+apt-get install autoconf automake g++ libtool -y || sudo apt-get install autoconf automake g++ libtool -y
+echo Finish installing building components.
+
 echo Installing components...
-sudo npm install --save
+npm install --save || sudo npm install --save
 echo Completing the component installation.
+
 echo Starting the system...
-sudo node ./source/shard.js
+node ./source/shard.js || sudo node ./source/shard.js
