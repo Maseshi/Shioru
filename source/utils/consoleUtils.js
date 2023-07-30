ba759c0c2f332445e22152d7387627484fa3c4f4
Maseshi <dermhioasw123@gmail.com>
2023-07-31 01:58:00 +0700

    🪛 Fixed an issue that didn't allow octal escape sequences.

--- source/utils/consoleUtils.js
+++ source/utils/consoleUtils.js
@@ -15,8 +15,8 @@ const ansiColor = (code, mode) => {
     if (code === null) return console.log("[ansiColor] Please configure the color of ANSI.");
     if (!mode) return console.log("[ansiColor] Please confirm the ANSI mode, including 'foreground', 'background' and 'sgr'.");
 
-    if (mode === "foreground") return "\033[38;5;" + code.toString() + "m";
-    if (mode === "background") return "\033[48;5;" + code.toString() + "m";
+    if (mode === "foreground") return "\x1b[38;5;" + code.toString() + "m";
+    if (mode === "background") return "\x1b[48;5;" + code.toString() + "m";
     if (mode === "sgr") return "\x1b[" + code.toString() + "m";
 }
 
