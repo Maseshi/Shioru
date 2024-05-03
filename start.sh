dd9be084769b032a7e7510c0e40424b13d8f83d7
Chaiwat Suwannarat <dermhioasw123@gmail.com>
2024-05-03 21:08:19 +0700

    🔘 Remove echo.

--- start.sh
+++ start.sh
@@ -14,14 +14,14 @@ echo # Will install: git, nodejs, default-jre,        #
 echo # python3, ffmpeg and build-essential           #
 echo #                                                #
 echo ##################################################
-echo.
+echo
 echo Updating system packages and installing required packages...
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install curl && curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y git nodejs default-jre python3 ffmpeg build-essential
-echo.
+echo
 echo Updating NPM to the latest version and installing components...
 sudo npm install -g npm@latest && sudo npm install
-echo.
+echo
 echo Starting up the system...
 sudo npm start
