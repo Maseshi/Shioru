50dc61ec309703368160a649812b0e298d796eb8
Chaiwat Suwannarat <dermhioasw123@gmail.com>
2024-05-03 19:58:38 +0700

    📁 Add files that do not need checking.

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,25 @@ const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommen
 
 module.exports = [
   {
+    ignores: [
+      '.note.txt',
+      '.firebase',
+      '.firebaserc',
+      'firebase.json',
+      'database.rules.json',
+      'firestore.indexes.json',
+      'firestore.rules',
+      'storage.rules',
+      '.env',
+      '.env.local',
+      '.env.development.local',
+      '.env.test.local',
+      '.env.production.local',
+      '.cache/',
+      'logs',
+      '*.log',
+      'node_modules/',
+    ],
     languageOptions: {
       globals: {
         node: true,
