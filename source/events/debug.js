205b4961f4d02897952d9d495a76d152eb4dd94d
codefactor-io <support@codefactor.io>
2025-05-27 13:26:05 +0000

    [CodeFactor] Apply fixes

--- source/events/debug.js
+++ source/events/debug.js
@@ -1,21 +1,21 @@
-const { Events, EmbedBuilder, Colors } = require('discord.js');
-const { webhookSend } = require('../utils/clientUtils');
-const { logger } = require('../utils/consoleUtils');
-const configs = require('../configs/data');
+const { Events, EmbedBuilder, Colors } = require("discord.js");
+const { webhookSend } = require("../utils/clientUtils");
+const { logger } = require("../utils/consoleUtils");
+const configs = require("../configs/data");
 
 module.exports = {
-	name: Events.Debug,
-	once: false,
-	execute(info) {
-		const webhookLogEmbed = new EmbedBuilder()
-			.setTimestamp()
-			.setColor(Colors.Yellow)
-			.setTitle('📜・Debug')
-			.setDescription(`\`\`\`${info}\`\`\``);
+  name: Events.Debug,
+  once: false,
+  execute(info) {
+    const webhookLogEmbed = new EmbedBuilder()
+      .setTimestamp()
+      .setColor(Colors.Yellow)
+      .setTitle("📜・Debug")
+      .setDescription(`\`\`\`${info}\`\`\``);
 
-		webhookSend(configs.logger.debug, {
-			embeds: [webhookLogEmbed],
-		});
-		logger.debug(info);
-	},
+    webhookSend(configs.logger.debug, {
+      embeds: [webhookLogEmbed],
+    });
+    logger.debug(info);
+  },
 };
